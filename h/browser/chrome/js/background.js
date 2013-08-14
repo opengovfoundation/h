af0cfc0f348d46668af8e37008ed2d5c7b07a3d2
Randall Leeds <tilgovi@hypothes.is>
2013-08-14 15:31:54 -0700

    [Chrome] work around restricted 3rd-party storage
    If the user has disabled 3rd-party cookies and storage in their Chrome
    settings then our iframe can't set cookies and can't read local storage
    because we're considered "3rd-party" so long as we're an iframe on a
    different origin than our own.
    
    This change asks for the 'contentSettings' permission, which provides
    the ability to change these settings programmatically. The extension
    now whitelists itself for storage, images, and javascript.

--- h/browser/chrome/js/background.js
+++ h/browser/chrome/js/background.js
@@ -57,6 +57,27 @@ function setPageAction(tabId, value) {
 
 
 function onInstalled() {
+  /* Make sure to enable localStorage and cookie storage for the extension.
+   * If the user has configured Chrome to block 3rd-party cookies and storage
+   * then the extension cannot function without this exception.
+   *
+   * Note that it'd be better if 'chrome-extension' scheme could be specified
+   * but the contentSettings API rejects these as invalid. Since the extension
+   * ID is a long, unique string with no TLD it should be safe to use a
+   * wildcard scheme. The only possible conflict would be if the user has used
+   * this ID somehow in a hosts file hack.
+   */
+  debugger
+  var details = {
+    primaryPattern: '*://' + chrome.runtime.id + '/*',
+    setting: 'allow'
+  }
+
+  chrome.contentSettings.cookies.set(details)
+  chrome.contentSettings.images.set(details)
+  chrome.contentSettings.javascript.set(details)
+
+  /* Enable the page action on all tabs. */
   chrome.tabs.query({}, function (tabs) {
     for (var i in tabs) {
       var tabId = tabs[i].id
